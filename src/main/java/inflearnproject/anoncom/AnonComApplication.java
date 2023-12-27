99a2fdb762699d308401e139d1c220fce45b43ce
jongwonpc <c3392302@naver.com>
2023-12-27 20:21:35 +0900

    feat: 프로젝트 시작

---
+++ src/main/java/inflearnproject/anoncom/AnonComApplication.java
@@ -0,0 +1,13 @@
+package inflearnproject.anoncom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonComApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inflearnproject/anoncom/AnonComApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearnproject.anoncom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
