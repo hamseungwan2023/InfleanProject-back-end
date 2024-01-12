5b2bec6099b5308f820808eb016f24fc6e4d2284
jongwonpc <c3392302@naver.com>
2024-01-12 17:48:36 +0900

    refactor : dto 클래스명 리팩토링

--- src/main/java/inflearnproject/anoncom/post/dto/ResAddPostDto.java
+++ src/main/java/inflearnproject/anoncom/post/dto/ResAddPostDto.java
@@ -13,22 +13,22 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ResPostDto {
+public class ResAddPostDto {
 
     private long id;
     private String title;
     private String category;
     private LocalDateTime createdAt;
-    private UserEntity user;
+    private Long userId;
     private int finalLike;
 
-    public static ResPostDto buildResPostDto(UserEntity user, Post post){
-        ResPostDto dto = ResPostDto.builder()
+    public static ResAddPostDto buildResPostDto(UserEntity user, Post post){
+        ResAddPostDto dto = ResAddPostDto.builder()
                 .id(post.getId())
                 .title(post.getTitle())
                 .category(post.getCategory())
                 .createdAt(post.getCreatedAt())
-                .user(user)
+                .userId(user.getId())
                 .finalLike(post.getUserLike() - post.getUserDisLike())
                 .build();
         return dto;
