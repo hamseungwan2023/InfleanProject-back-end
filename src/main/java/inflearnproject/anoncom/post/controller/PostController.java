1a13d51d3a0ee3db06646a8944f590dacc53778c
jongwonpc <c3392302@naver.com>
2024-01-12 17:48:19 +0900

    refactor : 반환타입 dto로 변환

--- src/main/java/inflearnproject/anoncom/post/controller/PostController.java
+++ src/main/java/inflearnproject/anoncom/post/controller/PostController.java
@@ -3,21 +3,20 @@ package inflearnproject.anoncom.post.controller;
 import inflearnproject.anoncom.domain.Post;
 import inflearnproject.anoncom.domain.UserEntity;
 import inflearnproject.anoncom.post.dto.ReqAddPostDto;
-import inflearnproject.anoncom.post.dto.ResPostDto;
+import inflearnproject.anoncom.post.dto.ResAddPostDto;
 import inflearnproject.anoncom.post.service.PostService;
 import inflearnproject.anoncom.security.jwt.util.IfLogin;
 import inflearnproject.anoncom.security.jwt.util.LoginUserDto;
 import inflearnproject.anoncom.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static inflearnproject.anoncom.post.dto.ReqAddPostDto.*;
-import static inflearnproject.anoncom.post.dto.ResPostDto.buildResPostDto;
+import static inflearnproject.anoncom.post.dto.ResAddPostDto.buildResPostDto;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,12 +26,13 @@ public class PostController {
     private final PostService postService;
     private final UserRepository userRepository;
     @PostMapping("/api/postWrite")
-    public ResponseEntity<String> addPost(@IfLogin LoginUserDto userDto, @RequestBody ReqAddPostDto postDto){
+    public ResponseEntity<ResAddPostDto> addPost(@IfLogin LoginUserDto userDto, @RequestBody ReqAddPostDto postDto){
+        //TODO 로그인 안 되어있으면 에러 발생
         Post post = buildPost(postDto);
         UserEntity user = userRepository.findByEmail(userDto.getEmail());
-        System.out.println(userDto);
-        System.out.println(user);
+
         postService.savePost(user,post);
-        return ResponseEntity.ok().body("ok");
+        ResAddPostDto resAddPostDto = buildResPostDto(user, post);
+        return ResponseEntity.ok().body(resAddPostDto);
     }
 }
