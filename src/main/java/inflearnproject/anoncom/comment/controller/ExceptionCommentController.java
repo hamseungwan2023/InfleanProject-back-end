36825d378529f57e7d8fe6c927543b4c5f005f84
jongwonpc <c3392302@naver.com>
2024-01-14 08:44:55 +0900

    feat: 댓글 관련 에러 핸들링 컨트롤러 구현

---
+++ src/main/java/inflearnproject/anoncom/comment/controller/ExceptionCommentController.java
@@ -0,0 +1,25 @@
+package inflearnproject.anoncom.comment.controller;
+
+import inflearnproject.anoncom.comment.exception.NoCommentException;
+import inflearnproject.anoncom.comment.exception.NotSameUserException;
+import inflearnproject.anoncom.error.ErrorDTO;
+import inflearnproject.anoncom.post.controller.PostController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(assignableTypes = CommentController.class)
+public class ExceptionCommentController {
+
+    @ExceptionHandler(NoCommentException.class)
+    public ResponseEntity<ErrorDTO> handleNoCommentException(NoCommentException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTO("글자 수와 형식에 맞게 작성해주십시오"));
+    }
+
+    @ExceptionHandler(NotSameUserException.class)
+    public ResponseEntity<ErrorDTO> handleNotSameUserException(NotSameUserException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTO("글자 수와 형식에 맞게 작성해주십시오"));
+    }
+}
