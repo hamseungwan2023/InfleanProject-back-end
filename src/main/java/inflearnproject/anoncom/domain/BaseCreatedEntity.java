bf143848a557cc0b6a49399f4c43c1628a3a633d
jongwonpc <c3392302@naver.com>
2024-01-24 01:59:09 +0900

    refactor: 생성 시간과 마지막으로 수정된 시간 클래스 분리

--- src/main/java/inflearnproject/anoncom/domain/BaseCreatedEntity.java
+++ src/main/java/inflearnproject/anoncom/domain/BaseCreatedEntity.java
@@ -1,9 +1,10 @@
 package inflearnproject.anoncom.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -11,10 +12,9 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public class BaseTimeEntity extends BaseCreatedEntity {
+public class BaseCreatedEntity {
 
-
-
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 }
