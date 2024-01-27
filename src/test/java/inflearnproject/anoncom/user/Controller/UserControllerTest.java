0ad707e9aa6e0ff5e20d5a5d82b920a8ea9f14e2
jongwonpc <c3392302@naver.com>
2024-01-27 20:54:17 +0900

    test: userControllerTest 병합 충돌

---
+++ src/test/java/inflearnproject/anoncom/user/Controller/UserControllerTest.java
@@ -0,0 +1,14 @@
+package inflearnproject.anoncom.user.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void mergeConflict(){
+
+    }
+
+}
