41448c90c1b28120803d59f2f0c07f89ae4f828e
jongwonpc <c3392302@naver.com>
2024-01-30 08:14:49 +0900

    feat: 쪽지 자세히 볼 때 n+1문제 해결

--- src/main/java/inflearnproject/anoncom/note/repository/NoteRepository.java
+++ src/main/java/inflearnproject/anoncom/note/repository/NoteRepository.java
@@ -2,6 +2,15 @@ package inflearnproject.anoncom.note.repository;
 
 import inflearnproject.anoncom.domain.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface NoteRepository extends JpaRepository<Note,Long> {
+
+    @Override
+    @Query("select n from Note n join fetch n.sender where n.id = :id")
+    Optional<Note> findById(@Param("id") Long id);
+
 }
