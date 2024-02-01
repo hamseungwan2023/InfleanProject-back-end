7fe94e5574c5b358bf5958a0c3369c7fe04e2679
jongwonpc <c3392302@naver.com>
2024-02-01 18:32:04 +0900

    refactor: declareService 리팩토링

--- src/main/java/inflearnproject/anoncom/declareNote/service/DeclareNoteService.java
+++ src/main/java/inflearnproject/anoncom/declareNote/service/DeclareNoteService.java
@@ -4,6 +4,7 @@ import inflearnproject.anoncom.declareNote.dto.DeclareUserDto;
 import inflearnproject.anoncom.declareNote.repository.DeclareNoteRepository;
 import inflearnproject.anoncom.domain.DeclareNote;
 import inflearnproject.anoncom.domain.UserEntity;
+import inflearnproject.anoncom.user.exception.NoUserEntityException;
 import inflearnproject.anoncom.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -11,7 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import static inflearnproject.anoncom.error.ExceptionMessage.NO_SAME_INFO_USER_MESSAGE;
 
 @Service
 @Transactional
@@ -21,13 +22,13 @@ public class DeclareNoteService {
     private final DeclareNoteRepository declareNoteRepository;
     private final UserRepository userRepository;
 
-    public Page<DeclareNote> showAll(Pageable pageable){
+    public Page<DeclareNote> showAll(Pageable pageable) {
         return declareNoteRepository.findAllWithDetails(pageable);
     }
 
     public void declareUser(DeclareUserDto declareUserDto) {
         String declaredNickname = declareUserDto.getNickname();
-        UserEntity user = userRepository.findByNickname(declaredNickname).get();
+        UserEntity user = userRepository.findByNickname(declaredNickname).orElseThrow(() -> new NoUserEntityException(NO_SAME_INFO_USER_MESSAGE));
         user.blockUser();
     }
 }
