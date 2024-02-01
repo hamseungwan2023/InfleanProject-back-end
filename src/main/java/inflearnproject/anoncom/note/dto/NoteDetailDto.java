27c03603cec8f3bb0c117f41a4329d258d1951a1
jongwonpc <c3392302@naver.com>
2024-02-02 05:44:54 +0900

    refactor: noteDetailDto 필드명 변경

--- src/main/java/inflearnproject/anoncom/note/dto/NoteDetailDto.java
+++ src/main/java/inflearnproject/anoncom/note/dto/NoteDetailDto.java
@@ -3,14 +3,18 @@ package inflearnproject.anoncom.note.dto;
 import inflearnproject.anoncom.domain.Note;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class NoteDetailDto {
 
     private String nickname;
     private String content;
+    private LocalDateTime readDate;
 
     public NoteDetailDto(Note note) {
         this.nickname = note.getSender().getNickname();
         this.content = note.getContent();
+        this.readDate = note.getReceiveDate();
     }
 }
