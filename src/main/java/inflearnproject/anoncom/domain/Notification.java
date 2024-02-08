f5cf56478e02147ca4e3693a981f8ee413246321
jongwonpc <c3392302@naver.com>
2024-02-09 04:53:01 +0900

    feat: 공지 엔티티 추가

---
+++ src/main/java/inflearnproject/anoncom/domain/Notification.java
@@ -0,0 +1,33 @@
+package inflearnproject.anoncom.domain;
+
+import inflearnproject.anoncom.enumType.LocationType;
+import inflearnproject.anoncom.enumType.PostCategory;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private PostCategory category;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private LocationType location;
+
+}
