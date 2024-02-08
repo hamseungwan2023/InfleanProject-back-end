65d82999055983e691e7a27a1a940d4310ffb1b7
jongwonpc <c3392302@naver.com>
2024-02-09 06:47:53 +0900

    feat: 알림 엔티티 구현

---
+++ src/main/java/inflearnproject/anoncom/domain/Alarm.java
@@ -0,0 +1,28 @@
+package inflearnproject.anoncom.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Alarm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "alarm_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id") //받는 사람
+    private UserEntity user;
+
+    private String message;
+
+    private boolean isRead;
+}
